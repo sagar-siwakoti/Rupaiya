c0e32f393b51c072f21a684656dea4eb13ac1f2d
Sagar Siwakoti <49759588+sagar-siwakoti@users.noreply.github.com>
2019-10-23 21:08:29 +0545

    Update check.php

--- check.php
+++ check.php
@@ -1,19 +1,13 @@
 <?php
-
-include("functions.php");
-
-if(isset($_POST['userName']))
-{
-$name= mysqli_real_escape_string($conn, $_POST['userName']);
-$query="SELECT * from user where UserName='$name'";
-$result = $conn->query($query);
-if ($result->num_rows == 0) 
-{
-	echo "No Match Found";
+include ("functions.php");
+if (isset($_POST['userName'])) {
+    $name = mysqli_real_escape_string($conn, $_POST['userName']);
+    $query = "SELECT * from user where UserName='$name'";
+    $result = $conn->query($query);
+    if ($result->num_rows == 0) {
+        echo "No Match Found";
+    } else {
+        echo "<span style='color:red;font-weight: bold;'>You already registered with this email</span>";
+    }
 }
-else
-{
-	echo "<span style='color:red;font-weight: bold;'>You already registered with this email</span>";
-}
-} 
 ?>
